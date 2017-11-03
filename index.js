3858ac3e1ca267eeadc46cb3acaf295179833399
Lance Hunter <lancehunter@gmail.com>
2017-11-03 14:57:10 -0500

    Completed second part of exercise

--- index.js
+++ index.js
@@ -2,12 +2,24 @@
   'use strict';
   // YOUR CODE HERE
 
+  let stopButton = document.getElementById('stopButton');
+  let slowButton = document.getElementById('slowButton');
+  let goButton = document.getElementById('goButton');
+
   let buttons = document.getElementById('controls');
   let stopLight = document.getElementById('stopLight');
   let slowLight = document.getElementById('slowLight');
   let goLight = document.getElementById('goLight');
 
   buttons.addEventListener('click', changeLight);
+  stopButton.addEventListener('mouseenter', logMouseEnter);
+  stopButton.addEventListener('mouseleave', logMouseLeave);
+  slowButton.addEventListener('mouseenter', logMouseEnter);
+  slowButton.addEventListener('mouseleave', logMouseLeave);
+  goButton.addEventListener('mouseenter', logMouseEnter);
+  goButton.addEventListener('mouseleave', logMouseLeave);
+
+
 
   function changeLight() {
     if (event.target === stopButton) {
@@ -18,5 +30,14 @@
       goLight.classList.toggle('go');
     }
   }
+
+  function logMouseEnter() {
+    console.log(`Entered ${event.target.getAttribute('id')} button`);
+  }
+
+  function logMouseLeave() {
+    console.log(`Left ${event.target.getAttribute('id')} button`);
+  }
+
 
 })();
