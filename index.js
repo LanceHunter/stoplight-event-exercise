7c3dc49b4233aa7eb1241629216587638f15f8d0
Lance Hunter <lancehunter@gmail.com>
2017-11-03 14:49:27 -0500

    Completed first part of exercise

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 (function() {
   'use strict';
-
   // YOUR CODE HERE
+
+  let buttons = document.getElementById('controls');
+  let stopLight = document.getElementById('stopLight');
+  let slowLight = document.getElementById('slowLight');
+  let goLight = document.getElementById('goLight');
+
+  buttons.addEventListener('click', changeLight);
+
+  function changeLight() {
+    if (event.target === stopButton) {
+      stopLight.classList.toggle('stop');
+    } else if (event.target === slowButton) {
+      slowLight.classList.toggle('slow');
+    } else if (event.target === goButton) {
+      goLight.classList.toggle('go');
+    }
+  }
+
 })();
